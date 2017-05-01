78acb00b4ec889f18426b32e5c6dd7c8a140f552
Antonio Jesus <ajesus.etsii@gmail.com>
2017-05-01 20:32:02 +0200

    Moved get embed videos to his own functionality

--- web/main.js
+++ web/main.js
@@ -24,6 +24,7 @@ function handleUrl(event) {
         success: function(response) {
             displayLookALike(response.mainImage);
             getFeaturedImages(response.name);
+            getEmbedVideos(response.name);
         },
         error: function(jqXHR, textStatus, errorMessage) {
             console.log(errorMessage); // Optional
@@ -61,6 +62,7 @@ function fileUpload(file) {
         success: function(response) {
             displayLookALike(response.mainImage);
             getFeaturedImages(response.name);
+            getEmbedVideos(response.name);
         },
         error: function(jqXHR, textStatus, errorMessage) {
             console.log(errorMessage); // Optional
@@ -82,18 +84,6 @@ function displayPreview(file) {
 function displayLookALike(imagePath) {
 
     appendImageToElement('#look-a-like', 'look-a-like-img', imagePath, 'img-thumbnail img-responsive', 'Picture of the look a like person');
-
-    $.ajax({
-        url: "/embed/" + 'aaliyah ca pelle',
-        type: "GET",
-        contentType: false,
-        success: function(response) {
-            displayEmbedVideos(response.embedIds);
-        },
-        error: function(jqXHR, textStatus, errorMessage) {
-            console.log(errorMessage); // Optional
-        }
-    });
 }
 
 function getFeaturedImages(name) {
@@ -130,6 +120,31 @@ function displayFeaturedImages(imagePaths) {
     }
 }
 
+function getEmbedVideos(name) {
+
+    nameToSearch = name.replace(/-/g, " ");
+
+    $.ajax({
+        url: "/embed/" + nameToSearch,
+        type: "GET",
+        contentType: false,
+        success: function(response) {
+            displayEmbedVideos(response.embedIds);
+        },
+        error: function(jqXHR, textStatus, errorMessage) {
+            console.log(errorMessage); // Optional
+        }
+    });
+}
+
+function displayEmbedVideos(embedIds) {
+
+    for (var embedId in embedIds) {
+        var iframe = '<div class="col-xs-6"><iframe class="embed-responsive-item" src="http://www.pornhub.com/embed/' + embedIds[embedId] + '" scrolling="no"></iframe></div>';
+        $('#embedVideos').append(iframe);
+    }
+}
+
 function appendImageToElement(elementId, imgId, imgSrc, imgClasses, imgAlt) {
 
     var img = $('<img />', {
@@ -140,14 +155,6 @@ function appendImageToElement(elementId, imgId, imgSrc, imgClasses, imgAlt) {
     });
 
     img.appendTo($(elementId));
-}
-
-function displayEmbedVideos(embedIds) {
-
-    for (var embedId in embedIds) {
-        var iframe = '<div class="col-xs-6"><iframe class="embed-responsive-item" src="http://www.pornhub.com/embed/' + embedIds[embedId] + '" scrolling="no"></iframe></div>';
-        $('#embedVideos').append(iframe);
-    }
 }
 
 function moveUploadInputToTop() {
